809074a050b7851ffceea56929f4abbc539d40fb
AbdulazizRus <coderdim@gmail.com>
2024-07-25 22:53:38 +0500

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import ChatPage from './routes/chatPage/ChatPage.jsx'
+import DashboardPage from './routes/dashboardPage/DashboardPage.jsx'
+import Homepage from './routes/homepage/Homepage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      { path: '/dashboard', element: <DashboardPage /> },
+      { path: '/dashboard/chats/:id', element: <ChatPage /> },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
--- src/routes/siginPage/SiginPage.jsx
+++ src/routes/siginPage/SiginPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './siginPage.css'
 
 const SiginPage = () => {
