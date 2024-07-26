c2ecc7924d0fc544bd4416009d15755ef6c68d8b
AbdulazizRus <coderdim@gmail.com>
2024-07-26 11:45:12 +0500

    changes

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -5,7 +5,17 @@ import './Homepage.css'
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Link to={'/dashboard'}>Dashboard</Link>
+      <img src="/orbital.png" alt="" className='orbital' />
+      <div className="left">
+        <h1>NORA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Nora AI is a cutting-edge conversational AI platform, delivering
+          intelligent and human-like interactions for seamless communication.
+        </h3>
+        <Link to={'/dashboard'}>Get Started</Link>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
